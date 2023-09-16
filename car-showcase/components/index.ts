c0179c58cb35a8c516c8a38dcd9706e09ffb1a88
lzr247 <lazarconic@yahoo.com>
2023-09-16 16:42:02 +0200

    Added headlessUI. Created initial structure for filters. Finished SearchManufacturer component.

--- car-showcase/components/index.ts
+++ car-showcase/components/index.ts
@@ -2,5 +2,16 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
