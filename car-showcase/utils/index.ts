3ab7af2c63e533575db1a70011bdb243554b6fb9
lzr247 <lazarconic@yahoo.com>
2023-09-17 09:57:38 +0200

    Implemented endpoint for fetching cars. Created CarCard component. Improved CustomButton component. Created new interfaces.

---
+++ car-showcase/utils/index.ts
@@ -0,0 +1,32 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "a70b84836emsh0e25931bd904d11p113e15jsn3c5a0cbde7b7",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
